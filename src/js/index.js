470dc19e8148680581864bc01aacc17fd036eb35
Bluce <a1019521a@gmail.com>
2021-06-13 22:13:21 +0800

    first commit hr style: dotted

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import '../scss/style.scss';
+
+import '../../node_modules/bootstrap/js/dist/dropdown';
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: {
+    app: './src/js/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'js/[name].[hash:8].js'
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: 'vendor',
+          chunks: 'initial',
+          enforce: true
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+       
+        use: {
+          loader: "babel-loader",
+          options:{
+            presets:['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            // inject CSS to page
+            loader: 'style-loader'
+          }
+          , {
+            // translates CSS into CommonJS modules
+            loader: 'css-loader'
+          }, {
+            // Run postcss actions
+            loader: 'postcss-loader',
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: function () {
+                  return [
+                    require('autoprefixer')
+                  ];
+                }
+              }
+            }
+          }, {
+            // compiles Sass to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [    
+    new HtmlWebpackPlugin({
+        title:'空氣品質API_bootstrap-v5',
+        template: './src/index.html',
+        filename: 'main.html',
+        chunks:['vendor','app']
+    }),
+  ],
+  devtool: "source-map",
+  devServer:{
+    compress:true,
+    contentBase: path.join(__dirname, 'dist'),
+    port:5050
+  }
+}
