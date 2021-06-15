8d74d9cf80f9469c22614e7434cc168f324b2c23
Bluce <a1019521a@gmail.com>
2021-06-15 14:31:55 +0800

    問題 : ReferenceError: regeneratorRuntime is not defined 使用 @babel/plugin-transform-runtime 解決 | 問題 : Promise fulfilled 狀態下 取得 PromiseResult 之中的結果，使用 async await 再加 return 得到值 而不是 Promise Object

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,95 @@
 import '../scss/style.scss';
 import '../../node_modules/bootstrap/js/dist/dropdown';
 
-//require('dotenv').config();
+// 介面 Dom
+const selectdom = document.querySelector('.form-select');
+const countySelect = document.querySelector('#county');
+const SiteDetailed = document.querySelector('.area-detailed');
+const SiteName = document.querySelector('#area');
 
-//api
-const url = process.env.API;
-//const url ='https://data.epa.gov.tw/api/v1/aqx_p_432?format=json&api_key=f72fe7f3-5981-476c-a1e9-4561d54d7b22';
-const xhr = new XMLHttpRequest();
-function get() {
-  xhr.open('get', url, true);
-  xhr.send(null);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      console.log(xhr.responseText);
-    }
-  };
+
+//api 
+const url = `${process.env.URL}${process.env.API_KEY}`;
+
+
+/**
+ * api 擷取資料
+ * @param {*} method 
+ * @param {*} url 
+ * @returns 
+ */
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.responseType = 'json';//等於 JSON.parse(xhr.response);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        //console.log(xhr.response);         
+        resolve(xhr.response);
+      }
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      }
+    };
+    xhr.onerror = () => {
+      console.error(xhr.statusText);
+      reject('出現錯誤(Error)')
+    };
+    xhr.send(null);
+  });
+  return promise;
 }
+
+//資料
+const getData = async () => {
+  let data;
+  await sendHttpRequest('GET', url).then(res => {
+    //console.log(res.records);
+    data = res.records;
+    //console.log(data);
+    //console.log(typeof data); //data的類型object，已經被JSON.Parse() 轉成object
+  });
+  //console.log(data);
+  return data;
+};
+
+//UI 台灣所有地區
+// const areaUI = () =>{
+// }
+
+//區域handle
+function area() {
+  let temparr = [];
+  let result = getData().then(d => {
+    d.forEach(el => {
+      temparr.push(el.County);
+    });
+    let data = new Set(temparr);
+    return [...data];
+    //console.log([...result]);
+  });
+  //console.log(result);
+  areaUI(result);
+}
+
+async function areaUI(data) {
+
+  let str = await data.then(d => {
+    let temp = '<option value="" disabled selected>請選擇地區</option>';
+    d.forEach(el => {
+      temp += `<option value="${el}">${el}</option>`;
+    })
+    //console.log(d);
+    return temp
+  });
+
+ // console.log("str", str);
+  selectdom.innerHTML = str;
+}
+
+// init 
+document.addEventListener('DOMContentLoaded', function () {
+  area();
+
+})
--- webpack.config.js
+++ webpack.config.js
@@ -27,11 +27,15 @@ module.exports = {
       {
         test: /\.js$/i,
         exclude: /node_modules/,
-
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: [
+              '@babel/preset-env',
+              {
+                plugins:['@babel/plugin-transform-runtime']
+              }
+            ]
           }
         }
       },
